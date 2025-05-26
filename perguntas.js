7c28776d6ac3756206827a0b9f55c0bad2386c03
Yasmim Emanuelly <snak.santos@escola.pr.gov.br>
2025-05-26 08:52:50 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Historia',
+    'Em que ano acabou a Segunda Guerra Mundial?',
+    '1945'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é LOVE'
 )
